2e952d50de6fdfbccb2daa3dc87b7a452541363e
Ranjeet Thakur <imranjeet30@gmail.com>
2023-04-23 18:50:48 +0530

    express setup

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
 const server = http.createServer((req, res) => {
-    res.write('hello node');
-    res.end();
+    if(req.url === '/'){
+        res.writeHead(200, {'content-type':'text/html'});
+        fs.readFile('./pages/home.html', 'utf8', (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200, {'content-type':'text/html'});
+        fs.readFile('./pages/about.html', 'utf8', (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
+    }
+    else{
+        res.writeHead(200, {'content-type':'text/html'});
+        fs.readFile('./pages/404.html', 'utf8', (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        }) 
+    }
+    
 });
-server.listen(3000);
+console.log('hey')
+server.listen(PORT);
